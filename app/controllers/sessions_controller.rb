358490af39b33b79fe25983bac09c0391efb83e5
Sachin <s.sachin94@gmail.com>
2016-12-20 12:56:11 +0530

    booststrap flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,15 +9,18 @@ class SessionsController < ApplicationController
 		user = User.authenticate(params[:email], params[:password])
 		if user
 			session[:user_id] = user.id
-			redirect_to root_url, :notice => "Logged in!"
+			redirect_to root_url
+			flash[:success] = "Logged in !"
 		else
-			redirect_to root_url,	:notice => "Wrong email or password!"
+			redirect_to root_url
+			flash[:danger] = "Wrong email or password !"
 			
 		end
 	end
 
 	def destroy
 		session[:user_id] = nil
-		redirect_to root_url, :notice => "Logged out!"
+		redirect_to root_url
+		flash[:success] = "Logged out !"
 	end
 end
